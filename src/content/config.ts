9bd4f36d51b0ac0b6332d122d578a78615ba5daa
Robson Junior <robson.ga.junior@gmail.com>
2025-03-05 00:15:01 -0300

    fix: layoutprops

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,13 @@ const base = z.object({
 	title: z.string(),
 	description: z.string(),
 	draft: z.boolean().optional().default(false),
-	noindex: z.boolean().optional().default(false)
+	noindex: z.boolean().optional().default(false),
+	image: z.string().default(''),
 })
 
 const institutional = defineCollection({
 	type: 'data',
 	schema: base.extend({
-		ogImage: z.string().default(''),
-		slug: z.string().optional(),
 		content: z.object({}).optional()
 	})
 })
