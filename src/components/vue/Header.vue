2ddda3697e4b3dc9c5d584dc2c9f2d112f5f2fba
Robson Junior <robson.ga.junior@gmail.com>
2025-03-05 20:50:42 -0300

    fix: header component, split home header

---
+++ src/components/vue/Header.vue
@@ -0,0 +1,7 @@
+<template>
+	<header class="absolute top-0 z-10 flex w-full justify-center overflow-hidden">
+		<div class="flex flex-col w-full h-full items-center z-20 justify-between py-8 px-container">
+			<slot name="menuright" />
+		</div>
+	</header>
+</template>
