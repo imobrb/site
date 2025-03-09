a6ea8485c66af1faa0ec12a83c428d2b9491ba66
Robson Junior <robson.ga.junior@gmail.com>
2025-03-09 10:29:15 -0300

    chore: remove files

--- src/services/get-highlights.js
+++
@@ -1,9 +0,0 @@
-/*
-Segue a rota:
-
-https://riobranco.immobilissistemas.com.br/ws/src/public/$2y$10$kEhJJHESuaVEPXCONeNfjeOJr7HiPj31HINb1mKFeIwlQK1f7xQvW/imoveis/destaques?ordenacao=3&limite=5
-
-Neste endpoint você tem a opção de escolher o tipo de ordenação pelo parâmetro ordenacao onde o valor 1 ordena por data de inclusão decrescente, 2 ordena por valor em ordem crescente e 3 ordena por valor em ordem decrescente
-
-Já o parâmetro limite o valor passado define a quantidade de destaques que devem ser exibidas na home.
-*/
