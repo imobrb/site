eafa5b1020948fad7f16d8eb97d47d846278fa07
Robson Junior <robson.ga.junior@gmail.com>
2025-03-09 11:46:31 -0300

    feat: linkbutton and sectioncardlist used in the home

--- src/components/vue/SectionCardList.vue
+++ src/components/vue/SectionCardList.vue
@@ -1,13 +1,51 @@
-<template></template>
+<template>
+	<section class="flex flex-col gap-8 md:gap-16 w-full">
+		<div class="flex w-full justify-between h-fit">
+			<h3 class="text-heading-3 font-bold">
+				{{ props.title }}
+			</h3>
+
+			<template v-if="props.link && props.link.href">
+				<LinkButton
+					rounded
+					outlined
+					:label="props.link.label"
+					:href="props.link.href"
+					:icon="props.link.icon &&  props.link.icon.name ? props.link.icon.name : 'pi pi-arrow-right'"
+					:icon-pos="props.icon && props.icon.position ? props.icon.position : 'right'"
+					size="small"
+				/>
+			</template>
+		</div>
+
+		<div class="w-full !m-0 !p-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-x-14 md:gap-y-20">
+			<CardImovel />
+			<CardImovel />
+			<CardImovel />
+			<CardImovel />
+			<CardImovel />
+			<CardImovel />
+		</div>
+	</section>
+</template>
 <script setup>
-	defineProps({
+	import CardImovel from '@components/vue/CardImovel.vue'
+	import LinkButton from '@components/vue/LinkButton.vue'
+
+	const props = defineProps({
 		title: {
 			type: String,
-			required: true
+			// required: true
 		},
 		link: {
-			type: String,
-			required: true
-		}
+			type: Object
+		},
+		items: {
+			type: Array,
+			default(rawProps) {
+				return []
+			}
+			// required: true
+		},
 	})
 </script>
