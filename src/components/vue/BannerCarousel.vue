c8913226853d2737f51aaa3b9e12be42bb59a5eb
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2025-04-05 12:15:58 -0300

    update galleria

--- src/components/vue/BannerCarousel.vue
+++ src/components/vue/BannerCarousel.vue
@@ -1,26 +1,31 @@
 <template>
-    <Galleria
-        :value="banners"
-        :showThumbnails="false"
-        :showIndicators="false"
-        :circular="true"
-        :transition-interval="5000"
-        :auto-play="true"
-        :showItemNavigators="true"
-        :activeIndex="activeIndex"
-        @update:activeIndex="onActiveIndexChange"
-        :pt="{
-            'root': '2xl:-ml-[10%] 2xl:w-[120%]'
-        }"
-    >
-        <template #item="slotProps">
-            <img :src="slotProps.item.url" :alt="slotProps.item.alt" class="2xl:w-[120%]">
-        </template>
-    </Galleria>
+    <div>
+        <Galleria
+            :value="banners"
+            :showThumbnails="false"
+            :showIndicators="false"
+            :circular="true"
+            :transitionInterval="5000"
+            :autoPlay="true"
+            :showItemNavigators="true"
+            :activeIndex="activeIndex"
+            @update:activeIndex="onActiveIndexChange"
+            :pt="{
+                'root': '2xl:-ml-[10%] 2xl:w-[120%] my-4 md:my-8',
+                'itemNavigators': {
+                    class: 'bg-white/50 hover:bg-white/80 text-black p-2 rounded-full'
+                }
+            }"
+        >
+            <template #item="slotProps">
+                <img :src="slotProps.item.url" :alt="slotProps.item.alt" class="2xl:w-[120%]">
+            </template>
+        </Galleria>
+    </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import Galleria from 'primevue/galleria'
 
 const activeIndex = ref(0)
@@ -34,9 +39,5 @@ const banners = ref([
     { url: 'https://www.imobiliariariobranco.com.br/wp-content/themes/imobiliariariobranco/assets/images/acompanhe_o_contrato.png', alt: 'Banner 2' },
     { url: 'https://www.imobiliariariobranco.com.br/wp-content/themes/imobiliariariobranco/assets/images/alo_universitario.png', alt: 'Banner 3' }
 ])
-
-onMounted(() => {
-    console.log('Banners carregados:', banners.value)
-})
 </script>
 
