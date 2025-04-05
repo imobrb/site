541f5792fa9b49b6faf0ad338a0ef1a76ef522ba
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2025-04-05 11:08:44 -0300

    chore: update layout page busca

--- src/components/vue/BuscaPage.vue
+++ src/components/vue/BuscaPage.vue
@@ -1,61 +1,72 @@
 <template>
-    <div class="flex flex-col gap-8">
-        <SearchHeader />
-        <div class="px-container w-full py-8 min-h-full md:gap-8 gap-4 flex flex-col">
-            <Breadcrumb
+    <div class="w-full px-container flex flex-col gap-4 p-8">
+        <Breadcrumb
                 :current-page="currentPage"
             />
-            <div class="w-full flex flex-col gap-4">
-                <div class="flex flex-row md:justify-between">
-                <div class="flex gap-2">
-                    <IconField>
-                        <InputIcon class="pi pi-search" />
-                        <InputText placeholder="Busca por nome, código ou bairro... " class="md:w-96" />
-                    </IconField>
-                    <Button 
-                        :icon="showFilters ? 'pi pi-minus' : 'pi pi-plus'" 
-                        :label="showFilters ? 'Filtros' : 'Filtros'" 
-                        class="rounded-full" 
-                        outlined 
-                        @click="showFilters = !showFilters"
+        <div class="w-full flex flex-col gap-4">
+            <div class="flex flex-row md:justify-between">
+            <div class="flex gap-2">
+                <IconField>
+                    <InputIcon class="pi pi-search" />
+                    <InputText placeholder="Busca por nome, código ou bairro... " class="md:w-96" />
+                </IconField>
+                <Button 
+                    :icon="showFilters ? 'pi pi-minus' : 'pi pi-plus'" 
+                    :label="showFilters ? 'Filtros' : 'Filtros'" 
+                    class="rounded-full" 
+                    outlined 
+                    @click="showFilters = !showFilters"
+                />
+            </div>
+
+            <Select :options="ordem" optionLabel="name" optionValue="code" placeholder="Ordenar por" class="rounded-full md:w-56 p-variant-secondary" />
+                
+            </div><!-- Filtros -->
+            <Transition
+                enter-active-class="transition-all duration-300 ease-in-out"
+                enter-from-class="opacity-0 max-h-0 translate-y-2"
+                enter-to-class="opacity-100 max-h-[1000px] translate-y-0"
+                leave-active-class="transition-all duration-300 ease-in-out"
+                leave-from-class="opacity-100 max-h-[1000px] translate-y-0"
+                leave-to-class="opacity-0 max-h-0 translate-y-2"
+            >
+                <div v-if="showFilters" class="flex flex-wrap gap-2 w-full">
+                    <Select
+                        placeholder="Tipo de Negócio"
+                        :options="[]"
+                        class="w-auto p-variant-secondary"
+                    />
+                    <Select
+                        placeholder="Tipo de Imóvel"
+                        :options="[]"
+                        class="w-auto p-variant-secondary"
+                    />
+                    <Select
+                        placeholder="Cidade"
+                        :options="[]"
+                        class="w-auto p-variant-secondary"
+                    />
+                    <Select
+                        placeholder="Detalhes"
+                        :options="[]"
+                        class="w-auto p-variant-secondary"
                     />
                 </div>
-
-                <Select :options="ordem" optionLabel="name" optionValue="code" placeholder="Ordenar por" class="rounded-full md:w-56 p-variant-secondary" />
-                    
-                </div><!-- Filtros -->
-                <Transition
-                    enter-active-class="transition-all duration-300 ease-in-out"
-                    enter-from-class="opacity-0 max-h-0 translate-y-2"
-                    enter-to-class="opacity-100 max-h-[1000px] translate-y-0"
-                    leave-active-class="transition-all duration-300 ease-in-out"
-                    leave-from-class="opacity-100 max-h-[1000px] translate-y-0"
-                    leave-to-class="opacity-0 max-h-0 translate-y-2"
-                >
-                    <div v-if="showFilters" class="flex flex-wrap gap-2 w-full">
-                        <Select
-                            placeholder="Tipo de Negócio"
-                            :options="[]"
-                            class="w-auto p-variant-secondary"
-                        />
-                        <Select
-                            placeholder="Tipo de Imóvel"
-                            :options="[]"
-                            class="w-auto p-variant-secondary"
-                        />
-                        <Select
-                            placeholder="Cidade"
-                            :options="[]"
-                            class="w-auto p-variant-secondary"
-                        />
-                        <Select
-                            placeholder="Detalhes"
-                            :options="[]"
-                            class="w-auto p-variant-secondary"
-                        />
-                    </div>
-                </Transition>
+            </Transition>
+        </div>
+    </div>
+    <Divider class="m-0 p-0" />
+    <div class="flex flex-col gap-8">
+        <div class="px-container w-full py-8 min-h-full md:gap-8 gap-4 flex flex-col">
+            <div class="flex md:flex-row flex-col gap-4 md:justify-between md:items-end">
+            <div class="flex flex-col gap-1">
+                <span class="text-body-3 text-surface-600">Imóveis para comprar em</span>
+                <h1 class="text-heading-3 text-surface-800">Rio Branco</h1>
             </div>
+            <div class="flex flex-col">
+                <span class="text-body-1 text-surface-500">271 Imóveis encontrados nessa região.</span>
+            </div>
+        </div>
 
             <div class="grid m-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-x-14 md:gap-y-20 mb-16">
                 <CardImovel 
@@ -75,8 +86,8 @@
 
 <script setup>
 import Breadcrumb from '@components/vue/Breadcrumb.vue'
-import SearchHeader from '@components/vue/SearchHeader.vue'
 import CardImovel from '@components/vue/CardImovel.vue'
+import Divider from 'primevue/divider'
 import Select from 'primevue/select'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
