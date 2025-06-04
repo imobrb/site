7a6d8ab26d8537ec7f8eacb48c8b22e97149fc47
Robson Junior <robson.ga.junior@gmail.com>
2025-06-04 15:31:36 -0300

    chore: codestyle parserCardImovelData.js

--- src/utils/parserCardImovelData.js
+++ src/utils/parserCardImovelData.js
@@ -1,24 +1,43 @@
 import { API_URL } from '@/consts'
 import formatReal from '@utils/formatReal'
 
+const parseRentPrice = (data) => {
+	return typeof data.valorLocacao === 'string' ? data.valorLocacao : data.valorLocacao?.valordoaluguelmaximo ? data.valorLocacao?.valordoaluguelmaximo : null
+}
+
+const parseSalePrice = (data) => {
+	return typeof data.valorVenda === 'string' ? data.valorVenda : data.valorVenda.valordevendamaximo ? data.valorVenda.valordevendamaximo : null
+}
+
+const parseTransactionType = (data) => {
+	return typeof data.valorLocacao === 'string' || data.valorLocacao?.valordoaluguelmaximo ? 'Aluguel' : 'Venda'
+}
+
+const parseImageSrc = (data) => {
+	return typeof data.fotos === 'string' ? data.fotos : data.fotos[parseInt(Math.random() * data.fotos.length)]?.caminho
+}
+
+const parseTitle = (data) => {
+	return `${data.dadosBasicos.tipoimovel} ${data.dadosBasicos.condominioresidencialedificio ? `- ` + data.dadosBasicos.condominioresidencialedificio : ''}`
+}
+
+const parseSubTitle = (data) => {
+	return `${data.dadosBasicos.cidade} - ${data.dadosBasicos.bairro} ${data.valorLocacao ? ` - ` + data.dadosBasicos.endereco : ''}`
+}
+
 export default (data) => {
 	const code = data.dadosBasicos.codigo
-	const title = `${data.dadosBasicos.tipoimovel} ${data.dadosBasicos.condominioresidencialedificio ? `- ` + data.dadosBasicos.condominioresidencialedificio : ''}`
-	const subtitle = `${data.dadosBasicos.cidade} - ${data.dadosBasicos.bairro} ${data.valorLocacao ? ` - ` + data.dadosBasicos.endereco : ''}`
-	const imageSrc = typeof data.fotos === 'string' ? data.fotos : data.fotos[parseInt(Math.random() * data.fotos.length)]?.caminho
-	const transactionType = typeof data.valorLocacao === 'string' || data.valorLocacao?.valordoaluguelmaximo ? 'Aluguel' : 'Venda'
-	const price = Number(typeof data.valorLocacao === 'string' ? data.valorLocacao : data.valorLocacao?.valordoaluguelmaximo ? data.valorLocacao?.valordoaluguelmaximo : typeof data.valorVenda === 'string' ? data.valorVenda : data.valorVenda.valordevendamaximo ? data.valorVenda.valordevendamaximo : '')
-	const parsedPrice = formatReal(price)
+	const title = parseTitle(data)
 
 	return {
 		code: code,
 		title: title,
-		subtitle: subtitle,
-		price: parsedPrice,
-		transactionType: transactionType,
+		subtitle: parseSubTitle(data),
+		price: formatReal(Number(parseRentPrice(data) || parseSalePrice(data))),
+		transactionType: parseTransactionType(data),
 		image: {
 			alt: title,
-			src: `${API_URL}/img?id=${code}&tamanho=347x196&imagem=${imageSrc}`
+			src: `${API_URL}/img?id=${code}&tamanho=347x196&imagem=${parseImageSrc(data)}`
 		},
 		link: {
 			label: 'Ver imóvel',
